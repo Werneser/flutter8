c4d9424a2efcc8b0b4ff04a4392c3a44977c5c52
Misha <mishaladeny@gmail.com>
2025-11-08 15:59:10 +0300

    Изменение main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:flutter8/user_services/screens/user_appointments_page.dart';
+import 'shared/app_theme.dart';
+import 'services/screens/services_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CompletedRequest {
+  final String id;
+  final String serviceId;
+  final String name;
+  final String phone;
+  final String email;
+  final String comment;
+  final DateTime dateSubmitted;
 
-  // This widget is the root of your application.
+  CompletedRequest({
+    required this.id,
+    required this.serviceId,
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.comment,
+    required this.dateSubmitted,
+  });
+}
+
+class UserDataInherited extends InheritedWidget {
+  final String? userName;
+  final List<String> bookedServiceIds;
+  final List<CompletedRequest> completedRequests;
+  final void Function(String) onNameChanged;
+  final void Function(String) onBookService;
+  final void Function(List<String>) onSetAppointments;
+  final void Function(CompletedRequest) onAddCompletedRequest;
+
+  const UserDataInherited({
+    Key? key,
+    required this.userName,
+    required this.bookedServiceIds,
+    required this.completedRequests,
+    required this.onNameChanged,
+    required this.onBookService,
+    required this.onSetAppointments,
+    required this.onAddCompletedRequest,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  static UserDataInherited? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<UserDataInherited>();
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  bool updateShouldNotify(UserDataInherited oldWidget) {
+    return oldWidget.userName != userName ||
+        oldWidget.bookedServiceIds != bookedServiceIds ||
+        oldWidget.completedRequests != completedRequests;
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyApp extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  bool _dark = false;
+  String? _userName;
+  List<String> _bookedServiceIds = [];
+  List<CompletedRequest> _completedRequests = [];
 
-  void _incrementCounter() {
+  void _setName(String name) {
+    setState(() => _userName = name);
+  }
+
+  void _toggleTheme() {
+    setState(() => _dark = !_dark);
+  }
+
+  void _bookService(String serviceId) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (!_bookedServiceIds.contains(serviceId)) {
+        _bookedServiceIds.add(serviceId);
+      }
     });
   }
 
+  void _setAppointments(List<String> ids) {
+    setState(() => _bookedServiceIds = List.from(ids));
+  }
+
+  void _addCompletedRequest(CompletedRequest req) {
+    setState(() => _completedRequests.add(req));
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return UserDataInherited(
+      userName: _userName,
+      bookedServiceIds: _bookedServiceIds,
+      completedRequests: _completedRequests,
+      onNameChanged: _setName,
+      onBookService: _bookService,
+      onSetAppointments: _setAppointments,
+      onAddCompletedRequest: _addCompletedRequest,
+      child: MaterialApp(
+        title: 'Госуслуги Flutter Demo',
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: _dark ? ThemeMode.dark : ThemeMode.light,
+        home: ServicesPage(
+          onNavigateToDetail: (service) {
+            // деталь будет открываться внутри ServicesPage
+          },
+          onThemeToggle: _toggleTheme,
         ),
+        routes: {
+          '/appointments': (_) => UserAppointmentsPage(),
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
