b755fbae79ef6a4be1d12e5f64685074d6327bdc
Werneser <mishaaldeny@gmail.com>
2025-11-15 11:02:39 +0300

    Обновление кода main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter8/shared/locator.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import 'shared/locator.dart';
 import 'package:get_it/get_it.dart';
+
 import 'shared/app_theme.dart';
 import 'services/screens/services_page.dart';
 import 'user_services/screens/user_appointments_page.dart';
+import 'shared/stores/app_state.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late AppState appState;
-  late VoidCallback _listener;
-
-  @override
-  void initState() {
-    super.initState();
-    appState = GetIt.instance<AppState>();
-    _listener = () => setState(() {});
-    appState.addListener(_listener);
-  }
-
-  @override
-  void dispose() {
-    appState.removeListener(_listener);
-    super.dispose();
-  }
+class MyApp extends StatelessWidget {
+  final AppState appState = GetIt.instance<AppState>();
 
   @override
   Widget build(BuildContext context) {
-    // Используем AnimatedBuilder для автоматического перерисования при изменениях AppState
-    return AnimatedBuilder(
-      animation: appState,
-      builder: (context, child) {
-        return MaterialApp(
-          title: 'Госуслуги Flutter Demo',
-          theme: AppTheme.lightTheme,
-          darkTheme: AppTheme.darkTheme,
-          themeMode: appState.isDark ? ThemeMode.dark : ThemeMode.light,
-          home: ServicesPage(
-            onNavigateToDetail: (service) {
-              // деталь будет открываться внутри ServicesPage
-            },
-            onThemeToggle: appState.toggleTheme,
-          ),
-          routes: {
-            '/appointments': (_) => UserAppointmentsPage(),
-          },
-        );
-      },
+    return Observer(
+      builder: (_) => MaterialApp(
+        title: 'Госуслуги Flutter Demo',
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: appState.isDark ? ThemeMode.dark : ThemeMode.light,
+        home: ServicesPage(
+          onNavigateToDetail: (service) {},
+          onThemeToggle: appState.toggleTheme,
+        ),
+        routes: {
+          '/appointments': (_) => UserAppointmentsPage(),
+        },
+      ),
     );
   }
 }
