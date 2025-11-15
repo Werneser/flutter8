d1fecef3deb9bdf7e1c7a0c8caa7d77b44b8ee70
Werneser <mishaaldeny@gmail.com>
2025-11-15 11:03:44 +0300

    Обновление локатора

--- lib/shared/locator.dart
+++ lib/shared/locator.dart
@@ -1,14 +1,12 @@
+import 'package:flutter8/shared/stores/app_state.dart';
 import 'package:get_it/get_it.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 final GetIt getIt = GetIt.instance;
 
 Future<void> setupLocator() async {
   getIt.registerLazySingleton<AppState>(() => AppState());
-
 }
-
 
 class CompletedRequest {
   final String id;
@@ -28,43 +26,4 @@ class CompletedRequest {
     required this.comment,
     required this.dateSubmitted,
   });
-}
-
-class AppState extends ChangeNotifier {
-  bool _isDark = false;
-  String? _userName;
-  List<String> _bookedServiceIds = [];
-  List<CompletedRequest> _completedRequests = [];
-
-  bool get isDark => _isDark;
-  String? get userName => _userName;
-  List<String> get bookedServiceIds => List.unmodifiable(_bookedServiceIds);
-  List<CompletedRequest> get completedRequests => List.unmodifiable(_completedRequests);
-
-  void setName(String name) {
-    _userName = name;
-    notifyListeners();
-  }
-
-  void toggleTheme() {
-    _isDark = !_isDark;
-    notifyListeners();
-  }
-
-  void bookService(String serviceId) {
-    if (!_bookedServiceIds.contains(serviceId)) {
-      _bookedServiceIds.add(serviceId);
-      notifyListeners();
-    }
-  }
-
-  void setAppointments(List<String> ids) {
-    _bookedServiceIds = List.from(ids);
-    notifyListeners();
-  }
-
-  void addCompletedRequest(CompletedRequest req) {
-    _completedRequests.add(req);
-    notifyListeners();
-  }
 }
